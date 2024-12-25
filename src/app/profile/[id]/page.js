e34b0663994c470dfb2fd1db283b24182244ff9a
kunal-2308 <kunaltaware210@gmail.com>
2024-12-25 21:28:14 +0530

    Getting params from URL

---
+++ src/app/profile/[id]/page.js
@@ -0,0 +1,9 @@
+export default function userProfile({params}){
+    let {id}=params;
+    return(
+        <div className="div-main-user-prfile-container flex flex-col justify-center items-center mt-20 gap-y-5">
+            <h1>Profile</h1>
+            <p className=" text-3xl">This is the profile of <span className="bg-orange-500 p-2 text-black rounded-lg">{id}</span></p>
+        </div>
+    )
+}
---
+++ src/app/profile/page.js
@@ -0,0 +1,8 @@
+export default function profile(){
+    return(
+        <div className="div-main-profile-container flex flex-col justify-center items-center">
+            <h1>Profile</h1>
+            
+        </div>
+    )
+}
